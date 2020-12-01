93c7a8fadad56ea318450b5abda8ade1540ddbb7
Levi.Ding <dingyankui@troila.com>
2020-12-01 16:09:53 +0800

    Initial commit

---
+++ src/test/java/cn/com/schedule/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.schedule.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
